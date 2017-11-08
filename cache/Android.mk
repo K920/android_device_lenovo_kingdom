0d6763629e157c359557bf57a6af5f4635a8c029
Michal Chvila <electrydev@gmail.com>
2017-11-08 22:36:59 +0100

    [DO NOT MERGE] kingdom: Do not specify we have cache partition
    * Due to the build failing to complete
    
    Change-Id: Id59cb0e00622da2fcee1aed033a6426e9835f6cd
    Signed-off-by: Michal Chvila <electrydev@gmail.com>

---
+++ cache/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := .electry
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_CACHE)
+LOCAL_MODULE_CLASS := ETC
+include $(BUILD_PREBUILT)
