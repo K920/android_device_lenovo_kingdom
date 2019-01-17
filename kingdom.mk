801edac3e17d56b967957ffa14961cb67064ffe6
Jan Dula <frantisheq@gmail.com>
2019-01-17 22:51:28 +0100

    kingdom: Make UI great again
    using values from stock build.prop
    
    Change-Id: I4c3b861eaa36c4a4735713e7b9fe13f7d8bf673a

--- kingdom.mk
+++ kingdom.mk
@@ -92,12 +92,12 @@ PRODUCT_COPY_FILES += \
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.checkjni=false \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=12m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
-    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapminfree=16m \
+    dalvik.vm.heapmaxfree=32m \
     dalvik.vm.verify-bytecode=false \
     persist.sys.purgeable_assets=1
 
@@ -170,6 +170,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@2.0-service
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
 
 # IPC Security config
 PRODUCT_COPY_FILES += \
@@ -356,12 +370,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf \
     wcnss_service
-
-# Higher fling velocities to smooth scrolling
-# and provide better responsiveness
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min.fling_velocity=160 \
-    ro.max.fling_velocity=20000
 
 PRODUCT_PACKAGES += qti-telephony-common
 PRODUCT_BOOT_JARS += telephony-ext
